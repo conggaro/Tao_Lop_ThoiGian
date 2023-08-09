0efefa080187a9577bce01363bb87a8d59d51f66
Công Nguyễn Chí <conggaro@gmail.com>
2023-08-09 11:36:11 +0700

    cap nhat

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 namespace MyApp
 {
     // tạo lớp thời gian
+    // lớp thời gian này là loại 24h
     public class ThoiGian
     {
         public int ngay { get; set; }
@@ -150,10 +151,49 @@ namespace MyApp
             return ketQua;
         }
 
+        // hàm trả về hôm qua
+        public ThoiGian hom_qua()
+        {
+            ThoiGian ketQua = new ThoiGian();
+
+            // tạo mảng
+            // chứa tổng số ngày trong 1 tháng
+            // của từng tháng
+            // từ tháng 1 đến tháng 12
+            int[] arr = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            // kiểm tra năm nhuận
+            if (nam % 400 == 0 || (nam % 4 == 0 && nam % 100 != 0))
+            {
+                arr[1] = 29;
+            }
+
+            if (ngay == 1 && thang == 1)
+            {
+                ketQua.ngay = 31;
+                ketQua.thang = 12;
+                ketQua.nam = nam - 1;
+            }
+            else if (ngay == 1 && thang != 1)
+            {
+                ketQua.ngay = arr[thang - 2];
+                ketQua.thang = thang - 1;
+                ketQua.nam = nam;
+            }
+            else if (ngay != 1)
+            {
+                ketQua.ngay = ngay - 1;
+                ketQua.thang = thang;
+                ketQua.nam = nam;
+            }
+
+            return ketQua;
+        }
+
         // hàm trả về giây tiếp theo
         public ThoiGian giay_TiepTheo()
         {
-            ThoiGian ketQua = new ThoiGian();
+            ThoiGian ketQua = this;
 
             if (gio == 23 && phut == 59 && giay == 59)
             {
@@ -186,12 +226,55 @@ namespace MyApp
             return ketQua;
         }
 
+        // hàm trả về giây trước
+        public ThoiGian giay_Truoc()
+        {
+            ThoiGian ketQua = this;
+
+            if (gio == 0 && phut == 0 && giay == 0)
+            {
+                // gọi thêm cái hàm in ra hôm qua
+                ketQua = this.hom_qua();
+
+                ketQua.gio = 23;
+                ketQua.phut = 59;
+                ketQua.giay = 59;
+            }
+            else if (gio != 0 && phut == 0 && giay == 0)
+            {
+                ketQua.gio = gio - 1;
+                ketQua.phut = 59;
+                ketQua.giay = 59;
+            }
+            else if (phut != 0 && giay == 0)
+            {
+                ketQua.gio = gio;
+                ketQua.phut = phut - 1;
+                ketQua.giay = 59;
+            }
+            else if (giay != 0)
+            {
+                ketQua.gio = gio;
+                ketQua.phut = phut;
+                ketQua.giay = giay - 1;
+            }
+
+            return ketQua;
+        }
+
         // nạp chồng toán tử +
         // dùng để cộng giây
         // cái tham số n là giây nhé
         public static ThoiGian operator +(ThoiGian dt, long n)
         {
-            ThoiGian ketQua = dt;
+            ThoiGian ketQua = new ThoiGian();
+
+            ketQua.ngay = dt.ngay;
+            ketQua.thang = dt.thang;
+            ketQua.nam = dt.nam;
+            ketQua.gio = dt.gio;
+            ketQua.phut = dt.phut;
+            ketQua.giay = dt.giay;
 
             if (n == 0)
             {
@@ -201,6 +284,33 @@ namespace MyApp
             for (long i = 1; i <= n; i++)
             {
                 ketQua = ketQua.giay_TiepTheo();
+            }
+
+            return ketQua;
+        }
+
+        // nạp chồng toán tử -
+        // dùng để trừ giây
+        // cái tham số n là giây nhé
+        public static ThoiGian operator -(ThoiGian dt, long n)
+        {
+            ThoiGian ketQua = new ThoiGian();
+
+            ketQua.ngay = dt.ngay;
+            ketQua.thang = dt.thang;
+            ketQua.nam = dt.nam;
+            ketQua.gio = dt.gio;
+            ketQua.phut = dt.phut;
+            ketQua.giay = dt.giay;
+
+            if (n == 0)
+            {
+                return ketQua;
+            }
+
+            for (long i = 1; i <= n; i++)
+            {
+                ketQua = ketQua.giay_Truoc();
             }
 
             return ketQua;
@@ -239,6 +349,21 @@ namespace MyApp
             Console.WriteLine($"Ban dau: {dt7.ToString("hh:mm:ss")}\t\t\tLuc sau: {dt7.giay_TiepTheo().ToString("hh:mm:ss")}");
 
 
+            // in ra giây trước
+            Console.WriteLine("\n\n-------------------- IN RA GIAY TRUOC --------------------");
+            ThoiGian dt14 = new ThoiGian(19, 5, 2007, 0, 0, 0);
+            Console.WriteLine($"Ban dau: {dt14.ToString()}\t\tLuc sau: {dt14.giay_Truoc().ToString()}\n");
+
+            ThoiGian dt15 = new ThoiGian(1, 1, 1, 18, 0, 0);
+            Console.WriteLine($"Ban dau: {dt15.ToString("hh:mm:ss")}\t\t\tLuc sau: {dt15.giay_Truoc().ToString("hh:mm:ss")}");
+
+            ThoiGian dt16 = new ThoiGian(1, 1, 1, 23, 10, 40);
+            Console.WriteLine($"Ban dau: {dt16.ToString("hh:mm:ss")}\t\t\tLuc sau: {dt16.giay_Truoc().ToString("hh:mm:ss")}");
+
+            ThoiGian dt17 = new ThoiGian(1, 1, 1, 7, 20, 0);
+            Console.WriteLine($"Ban dau: {dt17.ToString("hh:mm:ss")}\t\t\tLuc sau: {dt17.giay_Truoc().ToString("hh:mm:ss")}");
+
+
             // in ra ngày mai
             Console.WriteLine("\n\n--------------------- IN RA NGAY MAI ---------------------");
             ThoiGian dt8 = new ThoiGian(31, 12, 2023);
@@ -253,16 +378,41 @@ namespace MyApp
             ThoiGian dt11 = new ThoiGian(3, 3, 2003);
             Console.WriteLine($"Ban dau: {dt11.ToString("dd/MM/yyyy")}\t\tLuc sau: {dt11.ngay_mai().ToString("dd/MM/yyyy")}");
 
+            // in ra hôm qua
+            Console.WriteLine("\n\n---------------------- IN RA HOM QUA ----------------------");
+            ThoiGian dt18 = new ThoiGian(1, 1, 2023);
+            Console.WriteLine($"Ban dau: {dt18.ToString("dd/MM/yyyy")}\t\tLuc sau: {dt18.hom_qua().ToString("dd/MM/yyyy")}");
+
+            ThoiGian dt19 = new ThoiGian(1, 3, 2020);
+            Console.WriteLine($"Ban dau: {dt19.ToString("dd/MM/yyyy")}\t\tLuc sau: {dt19.hom_qua().ToString("dd/MM/yyyy")}");
+
+            ThoiGian dt20 = new ThoiGian(1, 3, 2019);
+            Console.WriteLine($"Ban dau: {dt20.ToString("dd/MM/yyyy")}\t\tLuc sau: {dt20.hom_qua().ToString("dd/MM/yyyy")}");
+
+            ThoiGian dt21 = new ThoiGian(15, 6, 2023);
+            Console.WriteLine($"Ban dau: {dt21.ToString("dd/MM/yyyy")}\t\tLuc sau: {dt21.hom_qua().ToString("dd/MM/yyyy")}");
+
 
             // nạp chồng toán tử +
             Console.WriteLine("\n\n------------------- NAP CHONG TOAN TU + -------------------");
-            ThoiGian dt12 = new ThoiGian(1, 1, 2000, 0, 0, 0);
+            ThoiGian dt12 = new ThoiGian(1, 1, 2000, 0, 0, 1);
             ThoiGian tong1 = dt12 + 86400;
             Console.WriteLine($"{dt12.ToString("dd/MM/yyyy hh:mm:ss")}    +    {"86400 (giay)", -15} =    {tong1.ToString("dd/MM/yyyy hh:mm:ss")}");
 
-            ThoiGian dt13 = new ThoiGian(22, 9, 2021, 0, 0, 0);
-            ThoiGian tong2 = dt13 + 0;
-            Console.WriteLine($"{dt13.ToString("dd/MM/yyyy hh:mm:ss")}    +    {"0 (giay)", -15} =    {tong2.ToString("dd/MM/yyyy hh:mm:ss")}");
+            ThoiGian dt13 = new ThoiGian(22, 9, 2021, 4, 8, 55);
+            ThoiGian tong2 = dt13 + 5;
+            Console.WriteLine($"{dt13.ToString("dd/MM/yyyy hh:mm:ss")}    +    {"5 (giay)", -15} =    {tong2.ToString("dd/MM/yyyy hh:mm:ss")}");
+
+
+            // nạp chồng toán tử -
+            Console.WriteLine("\n\n------------------- NAP CHONG TOAN TU - -------------------");
+            ThoiGian dt22 = new ThoiGian(15, 2, 2000, 10, 9, 48);
+            ThoiGian hieu1 = dt22 - 86400;
+            Console.WriteLine($"{dt22.ToString("dd/MM/yyyy hh:mm:ss")}    -    {"86400 (giay)",-15} =    {hieu1.ToString("dd/MM/yyyy hh:mm:ss")}");
+
+            ThoiGian dt23 = new ThoiGian(5, 12, 2021, 9, 20, 48);
+            ThoiGian hieu2 = dt23 - 1;
+            Console.WriteLine($"{dt23.ToString("dd/MM/yyyy hh:mm:ss")}    -    {"1 (giay)",-15} =    {hieu2.ToString("dd/MM/yyyy hh:mm:ss")}");
         }
     }
 }
